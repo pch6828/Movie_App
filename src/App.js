52a195ce8a13bc652010aba959459c18462c66d0
pch6828 <pch6828@hanyang.ac.kr>
2021-06-29 15:00:31 +0900

    6/29 : basic react study complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import {HashRouter, Route} from "react-router-dom";
 
-function App() {
-    return <div className = "App" / > ;
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation"
+
+import "./App.css"
+
+function App(){
+    return (
+        <HashRouter>
+            <Navigation/>
+            <Route path="/" exact={true} component={Home}/>
+            <Route path="/about" exact={true} component={About}/>
+            <Route path="/movie/:id" component={Detail}/>
+        </HashRouter>
+    );
 }
 
 export default App;
